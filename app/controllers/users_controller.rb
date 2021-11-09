e2798790b599f212ffaba7cdfbb0eba5274cc0c6
SarvarKhalimov <khalimovsarvar@gmail.com>
2021-11-09 16:31:15 +0500

    Create user index and create contollers, index view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,24 @@
 class UsersController < ApplicationController
+    def index
+        @users = User.all
+    end
+
     def new
         @user = User.new
+    end
+
+    def create()
+        @user = User.new(user_params)
+        if @user.save
+            redirect_to root_path
+        else
+            render :new
+        end
+    end
+
+    private
+    def user_params
+        params.require(:user)
+            .permit(:name, :position, :department, :telephone, :link)
     end
 end
