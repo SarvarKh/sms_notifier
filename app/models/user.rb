a183770c76aa9f8694e98ffabc98ee75af7273a6
SarvarKhalimov <khalimovsarvar@gmail.com>
2021-11-09 16:42:23 +0500

    Add callbacks to correct links with 'at'

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+    before_save :correct_twitter_url
+
+    def correct_twitter_url
+        self.link = self.link.include?("@") ? self.link.delete!("@") : self.link
+    end
 end
