a90b2e0936c83c23f7921d6487ec15ba569d7728
SarvarKhalimov <khalimovsarvar@gmail.com>
2021-11-16 19:51:46 +0500

    Create messages controller

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,32 @@
+class MessagesController < ApplicationController
+    before_action :authenticate_user!, except: [:index, :show]
+
+    def index
+        @messages = Message.all
+        @message = Message.new
+    end
+
+    def new
+        @message = current_user.messages.new
+    end
+
+    def create
+        @message = current_user.messages.build(message_params)
+    
+        respond_to do |format|
+          if @message.save
+            format.html { redirect_to root_path, notice: 'Message was successfully created.' }
+            format.json { render :show, status: :created, location: @message }
+          else
+            format.html { render :new }
+            format.json { render json: @message.errors, status: :unprocessable_entity }
+          end
+        end
+    end
+
+    private
+
+    def message_params
+        params.require(:message).permit(:message)
+    end
+end
