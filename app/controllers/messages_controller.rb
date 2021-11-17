72be78fd43b8403b757bf0506bf020d66a598a6a
SarvarKhalimov <khalimovsarvar@gmail.com>
2021-11-17 15:26:23 +0500

    Setup authorization

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,5 @@
 class MessagesController < ApplicationController
-    before_action :authenticate_user!, except: [:index, :show]
+    before_action :authenticate_user!
 
     def index
         @messages = Message.all
